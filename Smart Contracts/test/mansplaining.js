e68466bb5191772684cd1310fb24e29408531955
Alex <lepsalex@gmail.com>
2017-08-28 20:07:06 -0400

    Test expanded and all passing

--- Smart Contracts/test/mansplaining.js
+++ Smart Contracts/test/mansplaining.js
@@ -35,16 +35,15 @@ contract('Mansplaining', accounts => {
     });
 
     it("should enable a game to add four player", done => {
-
         MPGame.new(initPoints).then(gameInstance => {
             let testPromises = [];
-            let playerAddresses = [];
+            let activePlayers = [];
 
             testPromises.push(eventPromise(gameInstance.PlayerAdded(), result => {
-                playerAddresses.push(result.args);
+                activePlayers.push(result.args);
             }));
 
-            testPromises.push(gameInstance.addPlayer("lepsalex@gmail.com", "Alex", "f0f0f0"));
+            testPromises.push(gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0"));
             testPromises.push(gameInstance.addPlayer("one@gmail.com", "One", "a1a1a1"));
             testPromises.push(gameInstance.addPlayer("two@gmail.com", "Two", "b2b2b2"));
             testPromises.push(gameInstance.addPlayer("three@gmail.com", "Three", "c3c3c3"));
@@ -58,7 +57,7 @@ contract('Mansplaining', accounts => {
                     // ... if we run "testrpc -d -b 1" it will be 4 as it should be but damn will it be slow ...
                     setTimeout(() => {
                         event.stopWatching();
-                        assert.lengthOf(playerAddresses, 5, "Returned four player addresses");
+                        assert.lengthOf(activePlayers, 5, "Returned four player addresses");
                         done();
                     }, 1000);
                 });
@@ -68,15 +67,154 @@ contract('Mansplaining', accounts => {
     it("should not accept a fifth player", done => {
         const gameInstance = MPGame.new(initPoints).then(gameInstance => {
             let testPromises = [];
-            testPromises.push(gameInstance.addPlayer("lepsalex@gmail.com", "Alex", "f0f0f0"));
+            testPromises.push(gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0"));
             testPromises.push(gameInstance.addPlayer("one@gmail.com", "One", "a1a1a1"));
             testPromises.push(gameInstance.addPlayer("two@gmail.com", "Two", "b2b2b2"));
             testPromises.push(gameInstance.addPlayer("three@gmail.com", "Three", "c3c3c3"));
 
             Promise.all(testPromises)
                 .then(resolvedPromises => {
-                    expectThrow(gameInstance.addPlayer("nonono@gmail.com", "NONONO", "000000"));
+                    expectThrow(gameInstance.addPlayer("nonono@gmail.com", "NONONO", "000000"), done);
+                });
+        });
+    });
+
+    it("should start a game with at least one player", done => {
+        const gameInstance = MPGame.new(initPoints).then(gameInstance => {
+
+            gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0")
+            .then(() => {
+                let testPromises = [];
+
+                testPromises.push(eventPromise(gameInstance.GameReady()));
+                testPromises.push(gameInstance.startGame());
+                
+                return Promise.all(testPromises);
+            })
+            .then(resolvedPromises => {
+                const { event, result } = resolvedPromises[0];
+                event.stopWatching();
+                assert.equal(result.event, "GameReady", "Game state succesfully transitioned to ready state");
+                done();
+            });
+        });
+    });
+
+    it("should not allow game to start with no players", done => {
+        const gameInstance = MPGame.new(initPoints).then(gameInstance => {
+            expectThrow(gameInstance.startGame(), done);
+        });
+    });
+
+    it("should award specified player one point", done => {
+        MPGame.new(initPoints).then(gameInstance => {
+            let setupPromises = [];
+            let activePlayers = [];
+
+            setupPromises.push(eventPromise(gameInstance.PlayerAdded(), result => {
+                activePlayers.push(result.args);
+            }));
+
+            setupPromises.push(gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0"));
+            setupPromises.push(gameInstance.addPlayer("one@gmail.com", "One", "a1a1a1"));
+
+            Promise.all(setupPromises)
+                .then(resolvedPromises => {
+                    const { event } = resolvedPromises[0];
+                    event.stopWatching();
+                    return gameInstance.startGame();
+                })
+                .then(() => {
+                    const testPromises = [];
+                    testPromises.push(eventPromise(gameInstance.PointReceived()));
+                    testPromises.push(gameInstance.awardPoint(activePlayers[0].email));
+                    return Promise.all(testPromises);
+                })
+                .then(resolvedPromises => {
+                    const { event, result } = resolvedPromises[0];
+                    event.stopWatching();                    
+                    assert.equal(result.args.playerAddress, activePlayers[0].email, "Correct Player");
+                    assert.equal(result.args.playerScore, 1, "Player balance is now equal to 1");
                     done();
+                });
+        });
+    });
+
+    it("should not award points if game has not started", done => {
+        MPGame.new(initPoints).then(gameInstance => {
+            let setupPromises = [];
+            let activePlayers = [];
+
+            setupPromises.push(eventPromise(gameInstance.PlayerAdded(), result => {
+                activePlayers.push(result.args);
+            }));
+
+            setupPromises.push(gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0"));
+            setupPromises.push(gameInstance.addPlayer("one@gmail.com", "One", "a1a1a1"));
+
+            Promise.all(setupPromises)
+                .then(resolvedPromises => {
+                    const { event } = resolvedPromises[0];
+                    event.stopWatching();
+                    expectThrow(gameInstance.awardPoint(activePlayers[0].email), done);
+                });
+        });
+    });
+
+    it("should not award points if gamePoints are depleted", done => {
+        MPGame.new(0).then(gameInstance => {
+            let setupPromises = [];
+            let activePlayers = [];
+
+            setupPromises.push(eventPromise(gameInstance.PlayerAdded(), result => {
+                activePlayers.push(result.args);
+            }));
+
+            setupPromises.push(gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0"));
+            setupPromises.push(gameInstance.addPlayer("one@gmail.com", "One", "a1a1a1"));
+
+            Promise.all(setupPromises)
+                .then(resolvedPromises => {
+                    const { event } = resolvedPromises[0];
+                    event.stopWatching();
+                    return gameInstance.startGame()
+                })
+                .then(() => {
+                    expectThrow(gameInstance.awardPoint(activePlayers[0].email), done);
+                });
+        });
+    });
+
+    it("should return points balance and playerAddress given an email", done => {
+        MPGame.new(initPoints).then(gameInstance => {
+            let setupPromises = [];
+            let activePlayers = [];
+
+            setupPromises.push(eventPromise(gameInstance.PlayerAdded(), result => {
+                activePlayers.push(result.args);
+            }));
+
+            // Had to flip around order of these as the test readds the first player sometimes
+            // and results in zero balance even though it does actually work ... life on the frontier ...
+            setupPromises.push(gameInstance.addPlayer("one@gmail.com", "One", "a1a1a1"));
+            setupPromises.push(gameInstance.addPlayer("zero@gmail.com", "Zero", "f0f0f0"));
+
+            Promise.all(setupPromises)
+                .then(resolvedPromises => {
+                    const { event } = resolvedPromises[0];
+                    event.stopWatching();
+                    return gameInstance.startGame();
+                })
+                .then(() => {
+                    return gameInstance.awardPoint(activePlayers[0].email);
+                })
+                .then(() => {
+                    return gameInstance.getPlayerPoints("zero@gmail.com");
+                })
+                .then(response => {
+                    assert.equal(response[0], 1, "Player balance is now equal to 1");
+                    assert.isString(response[1], "Player address is returned");
+                    done(); 
                 });
         });
     });
