56c166413c7460075db800d9e65795695b220551
Alex <lepsalex@gmail.com>
2017-08-31 16:15:36 -0400

    Game engine placeholder

---
+++ Web/src/gameEngine/index.js
@@ -0,0 +1,11 @@
+/* Interface for our smart contracts */
+
+// Creates a new game and returns the newly minted contract address
+export function createNewGameContract() {
+    return new Promise(resolve => {
+        // mockup for now
+        setTimeout(() => {
+            resolve('0xfd09000c01309399d4c8507252d8cab950286ed8')
+        }, 2000)
+    })
+}
