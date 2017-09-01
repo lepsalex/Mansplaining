c765c4bae9052e4fd34376692781c6c9d6c79b2c
Alex <lepsalex@gmail.com>
2017-08-31 22:24:43 -0400

    Notes + WIP contracts integration

---
+++ Web/src/gameEngine/gameManager.js
@@ -0,0 +1,9 @@
+// Creates a new game and returns the newly minted contract address
+export function createNewGameContract() {
+    return new Promise(resolve => {
+        // mockup for now
+        setTimeout(() => {
+            resolve('0xfd09000c01309399d4c8507252d8cab950286ed8')
+        }, 100)
+    })
+}
--- Web/src/gameEngine/index.js
+++ Web/src/gameEngine/index.js
@@ -1,11 +1,6 @@
 /* Interface for our smart contracts */
 
-// Creates a new game and returns the newly minted contract address
-export function createNewGameContract() {
-    return new Promise(resolve => {
-        // mockup for now
-        setTimeout(() => {
-            resolve('0xfd09000c01309399d4c8507252d8cab950286ed8')
-        }, 100)
-    })
-}
+
+export { createNewGameContract } from './gameManager'
+
+export getWeb3 from './web3'
